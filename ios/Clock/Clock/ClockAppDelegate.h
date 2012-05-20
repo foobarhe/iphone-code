bae645c497a83ac770e6f549e40c8ebe8ba0887a
Min He <minjhe@gmail.com>
2012-05-20 21:22:45 +0800

    more

---
+++ ios/Clock/Clock/ClockAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  ClockAppDelegate.h
+//  Clock
+//
+//  Created by Yahoo on 5/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ClockViewController;
+
+@interface ClockAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ClockViewController *viewController;
+
+@end
