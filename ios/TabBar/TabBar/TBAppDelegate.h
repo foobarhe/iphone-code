ec71b229a2ea170d9e4b7260008af40f521fa4e9
Min He <minjhe@gmail.com>
2012-05-23 10:45:26 +0800

    TabBar example

---
+++ ios/TabBar/TabBar/TBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TBAppDelegate.h
+//  TabBar
+//
+//  Created by Yahoo on 5/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
