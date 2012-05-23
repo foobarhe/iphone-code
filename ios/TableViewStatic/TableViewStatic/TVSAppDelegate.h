97255926622d278095411e25dff5daf884cc01b9
Min He <minjhe@gmail.com>
2012-05-23 21:58:31 +0800

    static TableView

---
+++ ios/TableViewStatic/TableViewStatic/TVSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TVSAppDelegate.h
+//  TableViewStatic
+//
+//  Created by  on 5/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TVSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios/TableViewStatic/TableViewStatic/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TableViewStatic
+//
+//  Created by  on 5/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TVSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TVSAppDelegate class]));
+    }
+}
