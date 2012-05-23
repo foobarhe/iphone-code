8ec6b6d656904b5eb82e1caa36dda11ba8c8fa24
Min He <minjhe@gmail.com>
2012-05-23 22:03:43 +0800

    update static table view

---
+++ ios/TableViewStatic/TableViewStatic/TVSTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  TVSTableViewController.h
+//  TableViewStatic
+//
+//  Created by  on 5/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TVSTableViewController : UITableViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *name;
+@property (strong, nonatomic) IBOutlet UILabel *info;
+@property (strong, nonatomic) IBOutlet UIImageView *img;
+
+@end
