257bdf97043c26acab9cdc8676ecd213a60e1b2a
Min He <minjhe@gmail.com>
2012-05-25 13:20:51 +0800

    navigation without sb

---
+++ ios/NavigationNoSB/NavigationNoSB/DetailsTVTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailsTVTableViewController.h
+//  NavigationNoSB
+//
+//  Created by Yahoo on 5/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailsTVTableViewController : UITableViewController
+@property (strong, nonatomic) NSMutableArray *detailsData;
+@end
---
+++ ios/NavigationNoSB/NavigationNoSB/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NavigationNoSB
+//
+//  Created by Yahoo on 5/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NavAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NavAppDelegate class]));
+    }
+}
